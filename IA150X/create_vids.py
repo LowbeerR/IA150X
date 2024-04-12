68b0b5035b406ae09bae955762790070613cd7a7
LowbeerR <lowbeer@kth.se>
2024-04-12 11:27:47 +0200

    Create create_vids.py
    Add function to create static videos without hidden data in them

---
+++ IA150X/create_vids.py
@@ -0,0 +1,40 @@
+import random
+import imageio
+import numpy as np
+
+from PIL import Image
+
+width = 1280
+height = 700
+frame_amount = 600
+
+
+def bw_movie(video_filename):
+    frames = []
+    for i in range(frame_amount):
+        image = Image.new("1", (width, height))
+        pixels = image.load()
+
+        for x in range(width):
+            for y in range(height):
+                pixels[x, y] = (random.randint(0, 1))
+        image_rbg = image.convert("RGB")
+        image = np.array(image_rbg)
+        frames.append(image)
+        print(f"Frame {i + 1} of {frame_amount}")
+    imageio.mimsave(video_filename, frames, fps=10, codec="libx264")
+
+
+def rgb_movie(video_filename):
+    frames = []
+    for i in range(frame_amount):
+        image = Image.new("RGB", (width, height))
+        pixels = image.load()
+
+        for x in range(width):
+            for y in range(height):
+                pixels[x, y] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        image = np.array(image)
+        frames.append(image)
+        print(f"Frame {i + 1} of {frame_amount}")
+    imageio.mimsave(video_filename, frames, fps=10, codec="libx264")
