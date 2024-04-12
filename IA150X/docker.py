e4fa5c79de0580e30b9c5fb686d7e5a6e690e30a
LowbeerR <lowbeer@kth.se>
2024-04-12 17:20:08 +0200

    Create docker.py
    function run_isg to run docker/isg in python program

---
+++ IA150X/docker.py
@@ -0,0 +1,29 @@
+import time
+import winpty
+
+
+def run_isg(mode, path):
+    print("Starting ISG")
+    docker_path = r"C:\Program Files\Docker\Docker\resources\bin\docker.exe"
+    docker_command = [
+        docker_path,
+        "run",
+        "-it",
+        "--rm",
+        "-v",
+        "C:/Users/Rikard/Documents/GitHub/Infinite-Storage-Glitch:/home/Infinite-Storage-Glitch",
+        "isg",
+        "./target/release/isg_4real"
+    ]
+    process = winpty.PtyProcess.spawn(docker_command)
+    time.sleep(1)
+    process.write('\r')
+    time.sleep(1)
+    process.write(f'{path}\r')
+    time.sleep(1)
+    for i in range(mode):
+        process.write('\033[B')
+        time.sleep(1)
+    process.write('\r')
+    process.wait()
+    print("ISG terminated")
