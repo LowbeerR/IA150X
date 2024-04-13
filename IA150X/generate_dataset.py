04013c92573661ce572a7858fa76e3812ec7fc23
LowbeerR <lowbeer@kth.se>
2024-04-13 12:25:25 +0200

    Update generate_dataset.py
    No longer logs in terminal, no error print and no zip

--- IA150X/generate_dataset.py
+++ IA150X/generate_dataset.py
@@ -42,36 +42,38 @@ def generate_frames(save_location_path, file_name):
     try:
         os.mkdir(save_location_path)
     except OSError:
-        print("error")
+        pass
+        # print("error")
 
     count = count_files(save_location_path)
 
     if count <= 0:
         if "rbg_" in file_name:
-            ffmpeg.input(file_name).output(save_location_path + '\\rgb_frame%d.png', vframes=frames).run()
+            ffmpeg.input(file_name).output(save_location_path + '\\rgb_frame%d.png', vframes=frames, loglevel="quiet").run()
         else:
-            ffmpeg.input(file_name).output(save_location_path + '\\frame%d.png', vframes=frames).run()
+            ffmpeg.input(file_name).output(save_location_path + '\\frame%d.png', vframes=frames, loglevel="quiet").run()
     else:
         try:
             os.mkdir("data_temp")
         except OSError:
-            print("error")
+            pass
+            #print("error")
 
         temp_dir = os.path.join(os.getcwd(), "data_temp")
         if "rgb_" in file_name:
-            ffmpeg.input(file_name).output(temp_dir + '\\rgb_frame%d.png', vframes=frames).run()
+            ffmpeg.input(file_name).output(temp_dir + '\\rgb_frame%d.png', vframes=frames, loglevel="quiet").run()
             for file in os.listdir(temp_dir):
                 shutil.move(temp_dir + "\\" + file, target_dir + "\\rgb_frame%d.png" % count)
                 count += 1
         else:
-            ffmpeg.input(file_name).output(temp_dir + '\\frame%d.png', vframes=frames).run()
+            ffmpeg.input(file_name).output(temp_dir + '\\frame%d.png', vframes=frames, loglevel="quiet").run()
             for file in os.listdir(temp_dir):
                 shutil.move(temp_dir + "\\" + file, target_dir + "\\frame%d.png" % count)
                 count += 1
         os.rmdir(temp_dir)
 
 
-def zip_dataset(save_location_path, zip_y_n):
+def zip_dataset(save_location_path):
     move_location = os.path.join(os.getcwd(), "dataset")
     try:
         os.mkdir(move_location)
@@ -80,8 +82,6 @@ def zip_dataset(save_location_path, zip_y_n):
     generate_csv()
     shutil.move(os.path.join(os.getcwd(), save_location_path), move_location)
     shutil.move(os.path.join(os.getcwd(), "static_dataset.csv"), move_location)
-    if zip_y_n.lower() == "yes":
-        shutil.make_archive("dataset", "zip", os.path.join(os.getcwd(), "dataset"))
 
 
 def generate_frames_multiple_videos(save_location_path, path_to_video_folder):
@@ -89,16 +89,12 @@ def generate_frames_multiple_videos(save_location_path, path_to_video_folder):
     for file in os.listdir(path_to_video_folder):
         generate_frames(save_location_path, os.path.join(path_to_video_folder, file))
 
-    if input("generate dataset & CSV? (y/n)").lower() == "y":
-        try:
-            if input("zip dataset? (y/n)").lower() == "y":
-                y_or_n = "y"
-            zip_dataset(save_location_path, y_or_n)
-            print("dataset created!")
-        except OSError:
-            if input("zipped dataset already exists, remove existing dataset and create a new? (y/n)").lower() == "y":
-                shutil.rmtree('./dataset')
-                zip_dataset(save_location_path, y_or_n)
+    try:
+        zip_dataset(save_location_path)
+        print("dataset created!")
+    except OSError:
+        shutil.rmtree('./dataset')
+        zip_dataset(save_location_path)
 
 
 def get_fps(file_name):
