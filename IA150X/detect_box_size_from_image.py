b893db3274737f46ff99d055806301c7da3e303c
Emil Göransson <emilgo@kth.se>
2024-04-16 10:00:35 +0200

    Update detect_box_size_from_image.py
    new method to read images from videos, better performance

--- IA150X/detect_box_size_from_image.py
+++ IA150X/detect_box_size_from_image.py
@@ -1,17 +1,16 @@
 from csv import reader, DictReader
+import numpy as np
 import math
-import os
-import shutil
 import time
-import cv2
-import ffmpeg
 from pymediainfo import MediaInfo
+import av
 
 
 def check_box_same_color(size, array, x_pos, y_pos):
-    if x_pos + size > len(array[0]) or y_pos + size > len(array):
+    if x_pos + size > array.shape[1] or y_pos + size > array.shape[0]:
         return False
     color = array[y_pos][x_pos]
+
     for y in range(y_pos, y_pos + size):
         for x in range(x_pos, x_pos + size):
             if array[y][x] != color:
@@ -60,7 +59,7 @@ def find_box_size(imm_arr):
     size_limit = 4
     for size in range(1, size_limit + 1):
         box_found_count = 0
-        threshold = (pixels / (size * size)) * 0.01
+        threshold = (pixels / (size * size)) * threshold_varible_tuning
         for y in range(imm_arr.shape[0] - size + 1):
             for x in range(imm_arr.shape[1] - size + 1):
                 if no_adjacent_pixels_same_color(size, imm_arr, y, x):
@@ -68,59 +67,6 @@ def find_box_size(imm_arr):
                     if box_found_count >= threshold:
                         return True
     return False
-
-
-def video_to_frames(video_data, frames_checked_count, contains_data_threshold_ratio):
-    path, type, hidden_data = video_data
-    if 0 >= contains_data_threshold_ratio > 1:
-        raise Exception("contains_data_threshold_procent must be between 0 and 1")
-
-    temp_dir = os.path.join(os.curdir, "temp")
-    try:
-        os.mkdir(temp_dir)
-    except OSError:
-        shutil.rmtree(temp_dir)
-        os.mkdir(temp_dir)
-    total_fps = get_total_nr_frames(path)
-    freq = total_fps // frames_checked_count
-    # +4 to avoid first frames that contains "instructions" for ISG
-    frames_indices = [(4 + i * freq) % total_fps for i in range(frames_checked_count)]
-    for frame_index in frames_indices:
-        # % 30000 exists due to limitation in ffmpeg, finding a specific frame beyond 30 000 causes the program to crash
-        output_file = os.path.join(temp_dir, f"frame_{frame_index}.png")
-        (
-            ffmpeg
-            .input(path)
-            .filter('select', f'eq(n,{frame_index % 30000})')  # .format(frame_index))
-            .output(output_file, vframes=1, loglevel="quiet")
-            .run()
-        )
-    prediction = check_multiple_frames(temp_dir, frames_checked_count, contains_data_threshold_ratio)
-    if prediction != -1:
-        if prediction == int(hidden_data):
-            return True
-        else:
-            return False
-    else:
-        raise Exception("Error in check_multiple_frames")
-
-
-def check_multiple_frames(folder_path, frames_checked_count, contains_data_threshold_ratio):
-    frames_needed_to_contain_data = math.floor(frames_checked_count * contains_data_threshold_ratio)
-    frame = 0
-    has_box_count = 0
-    for image in os.listdir(folder_path):
-        # Sets limit on when a video is classified as hidden data
-        image_path = os.path.join(folder_path, image)
-        black_white = create_black_white_picture(image_path)
-        frame += 1
-        if find_box_size(black_white):
-            has_box_count += 1
-        if has_box_count >= frames_needed_to_contain_data:
-            return 1
-        if frame == frames_checked_count and has_box_count < frames_needed_to_contain_data:
-            return 0
-    return -1  # error
 
 
 def get_total_nr_frames(file_name):
@@ -160,13 +106,6 @@ def top_left_crop(img, target_width, target_height):
     return img
 
 
-def create_black_white_picture(image_path):
-    im_gray = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    im_gray_cropped = top_left_crop(im_gray, width_crop, height_crop)
-    (_, im_bw) = cv2.threshold(im_gray_cropped, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    return im_bw
-
-
 def test_videos_from_csv(file_csv, nr_of_frames_checked, data_ratio):
     time1 = time.time()
     with open(file_csv, "r", encoding="utf-8") as csvfile2:
@@ -192,7 +131,8 @@ def test_videos_from_csv(file_csv, nr_of_frames_checked, data_ratio):
             hidden_data = int(row['hidden_data'])
             video_info = (name, type, hidden_data)
             print(f"\r{100 * nr // tot_videos}%  │{'█' * nr}{'-' * (tot_videos - nr)}│", end='')
-            if video_to_frames(video_info, nr_of_frames_checked, data_ratio):
+            if video_to_frames(video_info, nr_of_frames_checked,
+                               data_ratio):  # video_to_frames(video_info, nr_of_frames_checked, data_ratio):
                 correct = correct + 1
                 if hidden_data == 1:
                     correct_per_type[type]['hidden_data'] = correct_per_type[type]['hidden_data'] + 1
@@ -211,12 +151,80 @@ def test_videos_from_csv(file_csv, nr_of_frames_checked, data_ratio):
               f"\nTime elapsed: {(time.time() - time1) / 60:.2f} minutes")
 
 
+# https://stackoverflow.com/questions/51285593/converting-an-image-to-grayscale-using-numpy and
+def black_white_conversion(image):
+    grayValue = 0.07 * image[:, :, 2] + 0.72 * image[:, :, 1] + 0.21 * image[:, :, 0]
+    img = grayValue.astype(np.uint8)
+    img[img > 128] = 255
+    img[img <= 128] = 0
+    return img
+
+
+def top_left_crop_alt(img, target_width, target_height):
+    (w, h, c) = img.shape
+    if w < target_width:
+        target_width = w
+    if h < target_height:
+        target_height = h
+    img = img[0:target_width, 0:target_height]
+    return img
+
+
+def detect_hidden_data_video(path, frames_checked_count, contains_data_threshold_ratio):
+    container = av.open(path)
+    # container.streams.video[0].thread_type = "AUTO"  # Go faster!
+
+    frames = 0
+    index = 0
+    has_box_count = 0
+    total_frames = container.streams.video[0].frames
+    if frames_checked_count > (total_frames - 4):
+        frames_checked_count = total_frames - 5
+    frames_needed_to_contain_data = math.floor(frames_checked_count * contains_data_threshold_ratio)
+
+    # +4 to avoid first frames that contains "instructions" for ISG
+
+    for frame in container.decode(video=0):
+        # Sets limit on when a video is classified as hidden data
+
+        if index > 4:
+            frames += 1
+            imm = frame.to_ndarray(format="rgb24")
+            imm = top_left_crop_alt(imm, width_crop, height_crop)
+            imm = black_white_conversion(imm)
+            if find_box_size(imm):
+                has_box_count += 1
+            if has_box_count >= frames_needed_to_contain_data:
+                return 1
+            if frames == frames_checked_count and has_box_count < frames_needed_to_contain_data:
+                return 0
+        else:
+            index += 1
+
+    return -1  # error
+
+
+def video_to_frames(video_data, frames_checked_count, contains_data_threshold_ratio):
+    path, type, hidden_data = video_data
+    if 0 >= contains_data_threshold_ratio > 1:
+        raise Exception("contains_data_threshold_procent must be between 0 and 1")
+    prediction = detect_hidden_data_video(path, frames_checked_count, contains_data_threshold_ratio)
+    if prediction != -1:
+        if prediction == int(hidden_data):
+            return True
+        else:
+            return False
+    else:
+        raise Exception(f"Error in check_multiple_frames, prediction: {prediction}")
+
+
 if __name__ == "__main__":
     # get list of coordinates that has boxes
-    nr_of_frames_to_be_checked = 10
-    contains_data_ratio = 0.9  # 10*0.9 = 9 frames needs to contain hidden_data to be classified as "hidden_data"
-    height_crop = 64
-    width_crop = 64
+
+    nr_of_frames_to_be_checked = 4
+    contains_data_ratio = 0.8  # 10*0.9 = 9 frames needs to contain hidden_data to be classified as "hidden_data"
+    height_crop = 32
+    width_crop = 32
+    threshold_varible_tuning = 0.03  # in find_box_size_numpy
+
     test_videos_from_csv("eval.csv", nr_of_frames_to_be_checked, contains_data_ratio)
-
-
