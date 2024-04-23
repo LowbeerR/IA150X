6cea5e4c9a07adc85926ef3cb6278e72c080d41e
LowbeerR <lowbeer@kth.se>
2024-04-23 10:02:44 +0200

    Update detect.py
    add more statistics

--- IA150X/detect.py
+++ IA150X/detect.py
@@ -35,6 +35,13 @@ if __name__ == "__main__":
             false_negative = 0
             total_correct_per_frame = 0
             total_frames = 0
+
+            static_bw_video_correct = []
+            static_rgb_video_correct = []
+            other_video_correct = []
+            static_bw_video_wrong = []
+            static_rgb_video_wrong = []
+            other_video_wrong = []
             correct_per_type = {'static_bw': {'no_hidden_data': 0, 'hidden_data': 0},
                                 'static_rgb': {'no_hidden_data': 0, 'hidden_data': 0},
                                 'other': {'no_hidden_data': 0, 'hidden_data': 0}}
@@ -43,6 +50,9 @@ if __name__ == "__main__":
                               'static_rgb': {'no_hidden_data': 0, 'hidden_data': 0},
                               'other': {'no_hidden_data': 0, 'hidden_data': 0}}
             nr = 0
+            other = 0
+            bw = 0
+            rgb = 0
             print("checking videos for hidden data")
             for row in reader:
                 name = row['name']
@@ -50,6 +60,8 @@ if __name__ == "__main__":
                 type = row['type'].replace(" ", "")
                 video_reader = torchvision.io.VideoReader(os.path.join("evaluation_dataset", name), "video")
                 hidden = 0
+                local_correct = 0
+                local_incorrect = 0
                 nr = nr + 1
                 n_samples = 0
                 print(f"\r{100 * nr // tot_videos}%  │{'█' * nr}{'-' * (tot_videos - nr)}│", end='')
@@ -67,13 +79,17 @@ if __name__ == "__main__":
                             hidden = hidden + 1
                             if label == 1:
                                 total_correct_per_frame += 1
+                                local_correct += 1
                             else:
                                 false_positive += 1
+                                local_incorrect += 1
                         elif predicted.item() == 0:
                             if label == 0:
                                 total_correct_per_frame += 1
+                                local_correct += 1
                             else:
                                 false_negative += 1
+                                local_incorrect += 1
                     total_frames += 1
                 if hidden / n_samples < 0.9:
                     if label == 0:
@@ -89,11 +105,32 @@ if __name__ == "__main__":
                     if label == 1:
                         correct = correct + 1
                         correct_per_type[type]['hidden_data'] = correct_per_type[type]['hidden_data'] + 1
+                if type == 'other':
+                    other_video_correct.append(local_correct)
+                    other_video_wrong.append(local_incorrect)
+                    other += 1
+                elif type == 'static_bw':
+                    static_bw_video_correct.append(local_correct)
+                    static_bw_video_wrong.append(local_incorrect)
+                    bw += 1
+                elif type == 'static_rgb':
+                    static_rgb_video_correct.append(local_correct)
+                    static_rgb_video_wrong.append(local_incorrect)
+                    rgb += 1
+
             print(f"\nNr of correct samples: {correct}, Number of false samples: {false}"
                   f"\nTotal correct frames: {total_correct_per_frame}, Number of false positives: {false_positive}, Number of false negatives: {false_negative}, Total amount of frames: {total_frames}"
                   f"\nNr of correct per type: static_bw = {correct_per_type['static_bw']}, static_rgb = {correct_per_type['static_rgb']}, other = {correct_per_type['other']}"
                   f"\nNr of false per type: static_bw = {false_per_type['static_bw']}, static_rgb = {false_per_type['static_rgb']}, other = {false_per_type['other']}"
                   f"\nTotal accuracy: {100 * correct / (correct + false):.0f}%"
-                  f"\nTime elapsed: {(time.time() - time1) / 60:.2f} minutes")
+                  f"\nTime elapsed: {(time.time() - time1) / 60:.2f} minutes"
+                  f"\nOther correct per video: {other_video_correct}"
+                  f"\nOther wrong per video: {other_video_wrong}"
+                  f"\nBW correct per video: {static_bw_video_correct}"
+                  f"\nBW wrong per video: {static_bw_video_wrong}"
+                  f"\nRGB correct per video: {static_rgb_video_correct}"
+                  f"\nRGB wrong per video: {static_rgb_video_wrong}"
+                  )
+
     except Exception as e:
         print(f"Error: {e}")
